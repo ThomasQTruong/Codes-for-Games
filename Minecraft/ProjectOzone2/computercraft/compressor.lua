707b9b0dfb248af52fe2d08f9975061faada3001
Thomas Truong <TruongThomasQ@gmail.com>
2025-05-10 03:25:04 -0700

    Created and coded compressor.lua

---
+++ Minecraft/ProjectOzone2/computercraft/compressor.lua
@@ -0,0 +1,25 @@
+--[[
+ - compressor.lua
+ - Compresses blocks (i.e. Cobblestone -> Compressed Cobblestone).
+ -
+ - Copyright (c) 2025, Thomas Truong.
+--]]
+
+local slots = {1, 2, 3, 5, 6, 7, 9, 10, 11}
+
+while true do
+  --- For every slot in a 3x3.
+  for _,slot in ipairs(slots) do
+    --- Select slot.
+    turtle.select(slot)
+    --- Get full stack of item for the slot from the storage BELOW the turtle.
+    while turtle.suckDown(64 - turtle.getItemCount()) do end
+  end
+
+  --- Craft compressed item.
+  turtle.craft()
+
+  --- Deposit compressed item to the top.
+  turtle.select(1)
+  turtle.dropUp()
+end
